1ba69e88b2d6384c76e09297637ce65c4d2b326a
SLLK <30961084+SLLK@users.noreply.github.com>
2017-08-20 21:07:51 -0500

    Create tsTest.py

---
+++ tsTest.py
@@ -0,0 +1,13 @@
+# Import modules
+import ev3dev.ev3 as ev3
+
+# Instantiate motors and sensors
+ts = ev3.TouchSensor()
+lm = ev3.LargeMotor('outA')
+
+# The Loop
+while True:
+  # Check if your holding down the touch sensor 
+  if(ts.value() == 1):
+  # Run the motor!
+    lm.run_timed(time_sp = 1, speed_sp = 1000)
